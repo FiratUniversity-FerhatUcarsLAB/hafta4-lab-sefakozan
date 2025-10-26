d1ab019fa49be748e3542910c862b6588f74f1cb
Sefa Kozan <sefakozan05@gmail.com>
2025-10-26 15:16:26 +0300

    Remove commented formatting examples

--- GeometrikHesap/src/GeometrikHesap.java
+++ GeometrikHesap/src/GeometrikHesap.java
@@ -8,17 +8,6 @@
   * daire ve kure ile ilgili geometrik hesaplamalari yaparak sonuclari formatli sekilde ekrana yazdirir.
   */
 
- /*
-  *         // %-30s  --> %s ile yazdırır, sola hizalı şekilde sağa boşluk
-  *         // %30s   --> %s ile yazdırır, sağa hizalı şekilde sola boşluk
-  *         // %7.3f  --> 3.14159   --> Çıktı: [  3.141] 7 karakter (. dahili)
-  *         // %-30.3f  --> 3.14159   --> Çıktı: [3.141`25 boşluk`] (. dahil)
-  *         // %.3s   --> Merhaba   --> Çıktı: [Mer] ilk 3 karakter
-  *         // %05d   --> 42        --> Çıktı: 00042
-  *         // %,d    --> 1234567   --> 1,234,567
-  *         // %+d    --> 42        --> +42
-  */
-
  import java.util.Scanner;
 
  public class GeometrikHesap {
